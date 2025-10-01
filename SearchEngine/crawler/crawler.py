9a827f45fd329a86e698e3ee93f1fdf6fb0678f4
Rafa <rafasuarzz@gmail.com>
2025-10-01 18:51:02 +0100

    Working test

--- SearchEngine/crawler/crawler.py
+++ SearchEngine/crawler/crawler.py
@@ -1,28 +1,32 @@
+from pathlib import Path
 import requests
-
 from .line_reader import read_content
 
 current_book_id = 0
+
 def crawler():
     global current_book_id
 
     downloaded_book = 0
-    while downloaded_book <4:
+    while downloaded_book < 4:
         url = f"https://www.gutenberg.org/cache/epub/{current_book_id}/pg{current_book_id}.txt"
-
         response = requests.get(url)
-        title = read_content(response.text)
 
         if response.status_code == 200:
-            download_path = r"C:C:\Users\sergi\OneDrive\Escritorio\UNI\Curso 2025-2026\Primer Cuatri\BD\Project\Playmaker-Apps-BD-\SearchEngine\datalake"
-            file_name = f"book_{title}.txt"
-            full_path = f"{download_path}\\{file_name}"
+            title = read_content(response.text)
 
-            with open(full_path, 'w', encoding='utf-8') as file:
+            # Ir al directorio raíz del proyecto y luego a Datalake
+            project_root = Path(__file__).resolve().parents[1]
+            download_path = project_root / "Datalake"
+            download_path.mkdir(exist_ok=True)
+
+            file_name = f"book_{title}.txt"
+            full_path = download_path / file_name
+
+            with open(full_path, "w", encoding="utf-8") as file:
                 file.write(response.text)
 
-
-
+            print(f"✅ Guardado en: {full_path}")
             downloaded_book += 1
-        current_book_id +=1
 
+        current_book_id += 1
